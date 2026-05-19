02ec22b0081f10bc489fbbf442828fbcd0c38753
Krishna Lokam <siva.lokam4work@gmail.com>
2026-05-19 16:32:15 +0530

    DSA with JS: pattern progragmming

---
+++ DSA/7.pattern_programming.js
@@ -0,0 +1,139 @@
+let prompt = require("prompt-sync")();
+
+/* 
+Problem 1:
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+*/
+// let n = Number(prompt("Enter the loop counter: "));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+/* 
+Problem 2: Right angled triangle
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+
+*/
+// let n = Number(prompt("Enter the loop counter: "));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+/* 
+Problem 3: Right angled triangle
+1 
+1 2  
+1 2 3
+1 3 3 4
+1 2 3 4 5
+*/
+
+// let n = Number(prompt("Enter the loop counter: "));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(j + " ");
+//   }
+//   console.log();
+// }
+
+/* 
+Problem 4: Right angled triangle
+A 
+A B
+A B C
+A B C D
+A B C D E
+*/
+// let n = Number(prompt("Enter the loop counter: "));
+// for (let i = 1; i <= n; i++) {
+//   let chaCode = 65;
+//   for (j = 1; j <= i; j++) {
+//     process.stdout.write(String.fromCharCode(chaCode++) + " ");
+//   }
+//   console.log();
+// }
+
+/* 
+Problem 5: Inverted Right angled triangle
+
+* * * * *
+* * * * 
+* * *
+* 
+
+*/
+// let n = Number(prompt("Enter the loop counter: "));
+// for (let i = n; i >= 1; i--) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+/* 
+Problem 6: Mirror right angled triangle
+          *
+        * *
+      * * *
+    * * * *
+  * * * * *
+*/
+// let n = Number(prompt("Enter the loop counter: "));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i; j++) {
+//     process.stdout.write("  ");
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+/* 
+Problem 7: Mirror right angled triangle
+     * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+*/
+// let n = Number(prompt("Enter the loop counter: "));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i; j++) {
+//     process.stdout.write(" ");
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+/* 
+Problem 8: Print X shape
+    
+*/
+let n = Number(prompt("Enter the loop counter: "));
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n; j++) {
+    if (i == j || i + j == n + 1) {
+      process.stdout.write("*");
+    } else {
+      process.stdout.write(" ");
+    }
+  }
+  console.log();
+}
