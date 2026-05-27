18e14a83d4ec6934f5d272cabfbc3ce76489853a
Krishna Lokam <siva.lokam4work@gmail.com>
2026-05-27 08:49:50 +0530

    Java: packages + Math

---
+++ DSA_JAVA/Sheryians/src/LangPackage.java
@@ -0,0 +1,13 @@
+public class LangPackage {
+    public static void main(String[] args) {
+        System.out.println(Math.min(10,12));
+        System.out.println(Math.abs(-10));
+        System.out.println((int)Math.floor(10.12));
+        System.out.println((int)Math.ceil(9.8));
+        System.out.println(Math.round(10.3));
+        System.out.println((int)Math.sqrt(100));
+        System.out.println((int)Math.cbrt(1000));
+        System.out.println((int)Math.sqrt(Math.pow(10,2)));
+        System.out.println(Math.round(11099.00112121));
+    }
+}
